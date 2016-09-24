f50943e08c24f437367047aed849c0f6f98d5bd2
Anton Linevich <anton@linevich.net>
2016-09-24 16:33:06 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='Pelican representative image plugin',
+      version='0.1',
+      url='https://github.com/linevich/representative_image',
+      packages=['representative_image',],
+)
